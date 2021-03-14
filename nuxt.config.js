2402fef930d0b3108845948cdcbe6534ac432f76
un-t-kitamura <s-kitamura@un-t.com>
2021-03-14 14:07:33 +0900

    setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const env = require(`./env.${environment}.js`)
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  mode: 'universal',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   // head: {
