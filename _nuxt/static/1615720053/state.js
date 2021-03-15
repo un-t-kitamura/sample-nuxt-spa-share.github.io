ee2b9636ce0dd4da5362c293688f5441e3e08eb6
un-t-kitamura <s-kitamura@un-t.com>
2021-03-15 16:12:27 +0900

    10d50a4

---
+++ _nuxt/static/1615720053/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsample-nuxt-spa-share.github.io\u002F_nuxt\u002Fstatic\u002F1615720053",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fsample-nuxt-spa-share.github.io\u002F",assetsPath:"\u002Fsample-nuxt-spa-share.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
