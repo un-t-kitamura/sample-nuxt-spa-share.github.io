8b2d88021ccb569f1cb6da193410d043c2acb4f0
un-t-kitamura <s-kitamura@un-t.com>
2021-03-19 15:15:48 +0900

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { loadNuxt, build } = require('nuxt')
 
 const app = require('express')()
 const isDev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 81
+const port = process.env.PORT || 3000
 
 async function start() {
   // Nuxt インスタンスを取得
