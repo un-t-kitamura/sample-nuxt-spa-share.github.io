7a64b2f27975b95ad5641edb8a64e107e8a1f387
un-t-kitamura <s-kitamura@un-t.com>
2021-03-19 19:04:26 +0900

    make it better

--- pages/content/_id.vue
+++ pages/content/_id.vue
@@ -34,7 +34,7 @@ export default {
           content: this.meta.description,
         },
         { hid: 'og:type', property: 'og:type', content: this.meta.type },
-        // { hid: 'og:title', property: 'og:title', content: this.meta.title },
+        { hid: 'og:title', property: 'og:title', content: this.meta.title },
         {
           hid: 'og:description',
           property: 'og:description',
